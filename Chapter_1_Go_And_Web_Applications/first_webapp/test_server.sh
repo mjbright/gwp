d32de7d6819c5ec5c5cebe2e42f2fbbd7954f3bc
Mike Bright <github@mjbright.net>
2015-09-27 15:35:59 +0200

    test now sends a series of GET/POST/HEAD requests to server

--- Chapter_1_Go_And_Web_Applications/first_webapp/test_server.sh
+++ Chapter_1_Go_And_Web_Applications/first_webapp/test_server.sh
@@ -2,9 +2,25 @@
 
 PORT=8080
 
+URL=http://localhost:${PORT}/Your_Name
+
+# Provide info on response headers:
+#INFO="-i"
+INFO=""
+
 echo
-#wget -O - http://localhost:${PORT}/Your_Name 2>/dev/null
-curl http://localhost:${PORT}/Your_Name
+#wget -O - $INFO ${URL} 2>/dev/null
+echo ">>>> Sending a GET request"
+curl -s ${INFO} ${URL}
+echo
+
+echo ">>>> Sending a POST request with data"
+curl -s --data "param1=value1&param2=value2" -X POST $INFO ${URL}
+echo
+
+echo ">>>> Sending a HEAD request"
+#curl -s -X HEAD -l $INFO ${URL}
+curl -s -I $INFO ${URL}
 echo
 
 
